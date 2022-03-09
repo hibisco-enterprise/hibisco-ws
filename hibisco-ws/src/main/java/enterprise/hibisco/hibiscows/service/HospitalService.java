25ccbb14ef241873c88ca86493cd32ef121af9d0
Gabriel-Prisco <gabriel.psilva@bandtec.com.br>
2022-03-09 14:15:41 -0300

    feat: adicionando services e métodos de login e registro do hospital

--- hibisco-ws/src/main/java/enterprise/hibisco/hibiscows/service/HospitalService.java
+++ hibisco-ws/src/main/java/enterprise/hibisco/hibiscows/service/HospitalService.java
@@ -1,5 +1,6 @@
 package enterprise.hibisco.hibiscows.service;
 
+import enterprise.hibisco.hibiscows.entities.Donator;
 import enterprise.hibisco.hibiscows.entities.Hospital;
 import enterprise.hibisco.hibiscows.entities.User;
 import org.springframework.stereotype.Service;
@@ -10,21 +11,41 @@ import java.util.List;
 @Service
 public class HospitalService {
 
-    private List<Hospital> hospitals;
+    private List<User> hospitals;
 
     public HospitalService() {
         this.hospitals = new ArrayList<>();
     }
 
-    public String doRegister(User donator) {
-        return "";
+    public String doRegister(User hospital) {
+        for (User user : hospitals) {
+            Hospital bloodCenter = ((Hospital) hospital);
+            Hospital hosp = ((Hospital) user);
+            if (bloodCenter.getCnpjHospital().equals(hosp.getCnpjHospital())){
+                return "CNPJ já existe no sistema!";
+            }
+        }
+        hospitals.add(hospital);
+        return "Usuário cadastrado com sucesso!";
     }
 
-    public String doLogin(String login, String password) {
-        return "";
+    public String doLogin(String email, String password) {
+        for (User user : hospitals) {
+            if (user.getEmail().equals(email) && user.recuperarPassword().equals(password)) {
+                user.setAuthenticated(true);
+                return "Login efetuado com sucesso!";
+            }
+        }
+        return "Usuário não encontrado. Tente novamente";
     }
 
     public String doLogoff(String email) {
-        return "";
+        for (User user : hospitals) {
+            if (user.getEmail().equals(email)) {
+                user.setAuthenticated(false);
+                return "Logoff efetuado com sucesso!";
+            }
+        }
+        return "Usuário não encontrado. Tente novamente";
     }
 }
