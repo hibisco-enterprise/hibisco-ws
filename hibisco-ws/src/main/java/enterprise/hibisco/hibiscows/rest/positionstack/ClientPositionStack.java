87631bc6fa645ea28caa1665f723c362de668247
casthegrinni <brunno.castegrini@hotmail.com>
2022-05-18 09:06:02 -0300

    fix(ClientPositionStack): url &

--- hibisco-ws/src/main/java/enterprise/hibisco/hibiscows/rest/positionstack/ClientPositionStack.java
+++ hibisco-ws/src/main/java/enterprise/hibisco/hibiscows/rest/positionstack/ClientPositionStack.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "positionstack", url = "http://api.positionstack.com/v1/forward?access_key=172bd50ab9e7fbf7f05e2565c08c5aa7&")
+@FeignClient(value = "positionstack", url = "http://api.positionstack.com/v1/forward?access_key=172bd50ab9e7fbf7f05e2565c08c5aa7")
 public interface ClientPositionStack {
     // http://api.positionstack.com/v1/forward?access_key=172bd50ab9e7fbf7f05e2565c08c5aa7&query=01414001%20Cerqueira%20Cesar%20Rua%20Haddock%20Lobo%20595,%20Sao%20Paulo%20SP
-    @GetMapping("query={address}")
+    @GetMapping("&query={address}")
     AllPositionStackResponse getAddress(@PathVariable String address);
 }
