0f1d74c402d678ebb52bf3e0688a12cefdb824ad
Gabriel-Prisco <gabriel.psilva@bandtec.com.br>
2022-06-05 02:13:25 -0300

    alteração de diretórios

--- hibisco-ws/src/main/java/enterprise/hibisco/hibiscows/repositories/DonationHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package enterprise.hibisco.hibiscows.repositories;
-
-import enterprise.hibisco.hibiscows.entities.DonationHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DonationHistoryRepository extends JpaRepository<DonationHistory, Long> {
-
-}
